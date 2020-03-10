4c4a693d6fd2126d5fdfadf576d4ad95af4f6764
William Chong <williamchong007@msn.com>
2020-03-11 00:50:29 +0800

    ✨ Put bookmark to local storage

--- src/Background/bookmarks.ts
+++ src/Background/bookmarks.ts
@@ -1,6 +1,12 @@
+import { browser } from 'webextension-polyfill-ts';
 import * as api from '../utils/api/likerland';
 
 let bookmarks = new Set<string>();
+
+export async function recoverLocalBookmark() {
+  const result = await browser.storage.local.get(['bookmarks']);
+  if (result.bookmarks) bookmarks = result.bookmarks;
+}
 
 export async function refreshBookmark() {
   const newBookmarks = await api.refreshBookmarkList();
@@ -9,18 +15,23 @@ export async function refreshBookmark() {
       return encodeURIComponent(url);
     })
   );
+  browser.storage.local.set({ bookmarks });
 }
 
 export async function addBookmark(url: string) {
   await api.addBookmark(url);
   bookmarks.add(encodeURIComponent(url));
+  browser.storage.local.set({ bookmarks });
 }
 
 export async function removeBookmark(url: string) {
   await api.removeBookmark(url);
   bookmarks.delete(encodeURIComponent(url));
+  browser.storage.local.set({ bookmarks });
 }
 
 export function isBookmarked(url: string): boolean {
   return bookmarks.has(encodeURIComponent(url));
 }
+
+recoverLocalBookmark();
